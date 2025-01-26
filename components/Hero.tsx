36b1060edc12aeef1cf1ebce804e2d38e2f4c33a
murashko12 <pert@ocumare.ru>
2025-01-26 17:42:25 +0300

    layout (header & footer)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 import CustomButton from './CustomButton'
 import Image from 'next/image'
 
-
 const Hero = () => {
 
     const handleScroll = () => {}
@@ -23,8 +22,8 @@ const Hero = () => {
             <div className="hero__image-container">
                 <div className="hero__image">
                     <Image src="/hero.png" alt="hero" fill className="object-contain" />
-                    <div className="hero__image-overlay"></div>
                 </div>
+                <div className="hero__image-overlay"></div>
             </div>
         </div>
     )
